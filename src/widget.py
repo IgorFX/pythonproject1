d318af49369d3a5a74796afae862968bb69f8c98
Igor <igorfx@mail.ru>
2025-04-16 05:26:50 +0600

    add module widget

---
+++ src/widget.py
@@ -0,0 +1,23 @@
+from masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(card: str) -> str:
+    """Возвращает название карты и маскиолваный номер карты(счета)"""
+    account_card = card.split()
+    if account_card[0] == "Счет":
+        masked_account = get_mask_account(int(account_card[1]))
+        return f"{account_card[0]} {masked_account}"
+    else:
+        if len(account_card) > 2:
+            card_name = f"{account_card[0]} {account_card[1]}"
+        else:
+            card_name = f"{account_card[0]}"
+        masked_card = get_mask_card_number(int(account_card[-1]))
+    return f"{card_name} {masked_card}"
+
+
+def get_date(date: str) -> str:
+    """Возвращает дату в формате ДД.ММ.ГГ"""
+    clear_date = date.split("T")[0].split("-")
+    new_data = f"{clear_date[2]}.{clear_date[1]}.{clear_date[0]}"
+    return new_data
